918e84faff7d3a4c7c3f4e660f1f6926c3edf0b1
lokesh <lokeshkumartcs2020@gmail.com>
2023-09-14 09:45:47 +0530

    Added files via Upload

--- Medium/src/Medium48.java
+++ Medium/src/Medium48.java
@@ -2,12 +2,27 @@ public class Medium48 {
 
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        for(int i = 0; i<n;i++){
-            for (int j =0; j<n;j++){
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
+        }
+
+        for(int i = 0;i<n;i++){
+            reverse(matrix[i]);
+        }
+
+    }
+
+    private void reverse(int[] arr){
+        int p1 = 0, p2 = arr.length - 1;
+        while(p1 < p2){
+            int temp = arr[p1];
+            arr[p1] = arr[p2];
+            arr[p2] = temp;
+            p1++;p2--;
         }
     }
 
@@ -15,12 +30,12 @@ public class Medium48 {
         Medium48 medium48 = new Medium48();
         int[][] matrix = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
         medium48.rotate(matrix);
-        for (int[] rows : matrix) {
-            for (int element : rows) {
-                System.out.print(element + " ");
-            }
-            System.out.println("");
-        }
+//        for (int[] rows : matrix) {
+//            for (int element : rows) {
+//                System.out.print(element + " ");
+//            }
+//            System.out.println("");
+//        }
     }
 
 }
